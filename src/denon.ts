1e76d1bc7d5d6660232d43d813443c42e5b1212e
Tyler Natsuhara <ty@ty.pizza>
2022-12-09 11:42:50 -0800

    improve API

--- src/denon.ts
+++ src/denon.ts
@@ -1,8 +1,9 @@
 import { Socket } from "net"
 
 const DENON_IP = process.env.ADDRESS!
+const COMMAND_TIMEOUT = 500
 
-type DenonResult = { error?: string; response?: string }
+type DenonResult = { error?: string; response?: string; status: number }
 
 export const sendCommand = async (command: string) => {
     const socket = new Socket({ allowHalfOpen: true })
@@ -13,26 +14,26 @@ export const sendCommand = async (command: string) => {
         socket.connect(23, DENON_IP, () => resolve())
     })
 
-    const result = new Promise<DenonResult>((resolve, reject) => {
+    const result = new Promise<DenonResult>((resolve) => {
         socket.write(command + "\r")
         if (!command.endsWith("?")) {
-            resolve({ response: `executed ${command}` })
+            resolve({ response: `executed ${command}`, status: 200 })
         }
         socket.on("data", (buffer) => {
             const response = buffer.toString().trim()
             console.log(`${command} -> ${response}`)
             socket.destroy()
             //this data is a Buffer object
-            resolve({ response })
+            resolve({ response, status: 200 })
         })
         socket.on("error", () => {
             socket.destroy()
-            reject({ error: true })
+            resolve({ error: "error response from receiver", status: 400 })
         })
     })
 
     const timeout = new Promise<DenonResult>((resolve) =>
-        setTimeout(() => resolve({ error: "timeout" }), 500)
+        setTimeout(() => resolve({ error: "timeout", status: 408 }), COMMAND_TIMEOUT)
     )
 
     return Promise.race([result, timeout])
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,22 @@
-import express from "express"
+import express, { Response } from "express"
 import { sendCommand } from "./denon"
 
 const PORT = process.env.PORT ?? 8000
 
 const app = express()
 
-app.get("/denon/get/:field", async (req, res) => {
+const commandHandler = async (cmd: string, res: Response) => {
     res.set("Access-Control-Allow-Origin", "*")
     try {
-        const responseData = await sendCommand(req.params.field.toUpperCase() + "?")
-        res.status(200).send(responseData)
+        const response = await sendCommand(cmd.toUpperCase())
+        res.status(response.status).send(response)
     } catch (error) {
         res.status(500).send({ error: "exception" })
     }
-})
+}
 
-app.get("/denon/set/:command", async (req, res) => {
-    res.set("Access-Control-Allow-Origin", "*")
-    try {
-        const responseData = await sendCommand(req.params.command.toUpperCase())
-        res.status(200).send(responseData)
-    } catch (error) {
-        res.status(500).send({ error: "exception" })
-    }
-})
+app.get("/denon/get/:field", async (req, res) => commandHandler(req.params.field + "?", res))
+app.get("/denon/set/:command", async (req, res) => commandHandler(req.params.command, res))
 
 app.listen(PORT)
 console.log(`listening on port ${PORT}`)
